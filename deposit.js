78e203f401f95d4ddd03efa3efd93faae3492c11
BidhanKm <bidhankm426@gmail.com>
2023-02-25 02:27:09 +0600

    Bank Account

--- deposit.js
+++ deposit.js
@@ -1,10 +1,32 @@
 document.getElementById('btn-deposit').addEventListener('click',function(){
     const depositInputField = document.getElementById('deposit-field');
-    const depositFIeld = depositInputField.value
+    const depositFieldString = depositInputField.value
+    const depositField = parseFloat(depositFieldString);
 
     const deposit = document.getElementById('total-deposit');
-    const totalDeposit = deposit.innerText
-    deposit.innerText = depositFIeld;
+    const totalDepositString = deposit.innerText
+    const totalDeposit = parseFloat(totalDepositString);
+
+    const currentBalance = depositField + totalDeposit;
+
+    deposit.innerText = currentBalance;
+    
+//  add balance
+
+const balance = document.getElementById('balance-field');
+const totalBalanceString = balance.innerText;
+const totalBalance = parseFloat(totalBalanceString)
+
+const currentTotalBalance = totalBalance + depositField;
+balance.innerText = currentTotalBalance;
+
+
+
+
+
+
+
+depositInputField.value = "";
+
 })
 
-//
